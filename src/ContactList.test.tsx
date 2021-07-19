b155f7e563d6128b2c859c845f27b3253a146be3
Bartłomiej Glac <bartlomiejglac@MacBook-Pro-Bartomiej.local>
2021-07-19 17:44:36 +0200

    Added test for loading indicator

--- src/ContactList.test.tsx
+++ src/ContactList.test.tsx
@@ -1,48 +1,51 @@
-import { render, screen } from "@testing-library/react";
+import {
+  screen,
+  render,
+  waitForElementToBeRemoved,
+  wait,
+  waitForElement,
+} from "@testing-library/react";
 import React from "react";
 import App from "./App";
 
 const mockPersonArray = [
   {
-    id: "1",
-    jobTitle: "Fabricator",
-    emailAddress: "Ron_Giles3711@dionrab.com",
-    firstNameLastName: "Ron Giles",
+    id: "151",
+    jobTitle: "Stockbroker",
+    emailAddress: "Vera_Janes1389@corti.com",
+    firstNameLastName: "Vera Janes",
   },
   {
-    id: "2",
-    jobTitle: "IT Support Staff",
-    emailAddress: "Melinda_Mcgregor7556@mafthy.com",
-    firstNameLastName: "Melinda Mcgregor",
+    id: "152",
+    jobTitle: "Inspector",
+    emailAddress: "Janelle_Chappell9153@zorer.org",
+    firstNameLastName: "Janelle Chappell",
   },
 ];
+
+jest.mock("./api", () =>
+  jest.fn().mockImplementation(() => Promise.resolve(mockPersonArray))
+);
 
 afterEach(() => {
   jest.clearAllMocks();
 });
 
 test("should render elements on the list when fetches successfully", async () => {
-  jest.mock("./api", () =>
-    jest.fn().mockResolvedValueOnce([
-      {
-        id: "1",
-        jobTitle: "Fabricator",
-        emailAddress: "Ron_Giles3711@dionrab.com",
-        firstNameLastName: "Ron Giles",
-      },
-      {
-        id: "2",
-        jobTitle: "IT Support Staff",
-        emailAddress: "Melinda_Mcgregor7556@mafthy.com",
-        firstNameLastName: "Melinda Mcgregor",
-      },
-    ])
-  );
   render(<App />);
-  expect(
-    await screen.findByText(mockPersonArray[0].emailAddress)
-  ).toBeInTheDocument();
-  expect(
-    await screen.findByText(mockPersonArray[1].emailAddress)
-  ).toBeInTheDocument();
+  await waitForElement(() => screen.getByText(mockPersonArray[0].emailAddress));
+  expect(screen.getByText(mockPersonArray[0].emailAddress)).toBeInTheDocument();
+  expect(screen.getByText(mockPersonArray[1].emailAddress)).toBeInTheDocument();
+});
+
+test("should render loading indicator when before fetching data", async () => {
+  render(<App />);
+  const findLoadingElement = () => screen.getByText("Loading...");
+  expect(findLoadingElement()).toBeInTheDocument();
+
+  await waitForElementToBeRemoved(() => findLoadingElement());
+
+  mockPersonArray.forEach((person) =>
+    expect(screen.getByText(person.emailAddress)).toBeInTheDocument()
+  );
 });
