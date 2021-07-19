400309a2b74e72a98d50b1f45390f17f365c164f
Bartłomiej Glac <bartlomiejglac@MacBook-Pro-Bartomiej.local>
2021-07-19 16:02:54 +0200

    Added implementation for fetching contacts

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import apiData from "./api";
 import PersonInfo from "./PersonInfo";
 
+interface UserProfile {
+  id: string;
+  jobTitle: string;
+  emailAddress: string;
+  firstNameLastName: string;
+}
+
 function App() {
-  const [data, setData] = React.useState([]);
+  const [data, setData] = React.useState<UserProfile[]>([]);
   const [selected, setSelected] = React.useState([]);
 
   //  TODO fetch contacts using apiData function, handle loading and error states
+  useEffect(() => {
+    apiData()
+      .then((batch) => setData(data.concat(batch)))
+      .finally(() => {});
+  }, []);
 
   return (
-    <div className="App">
-      <div className="selected">Selected contacts: {selected.length}</div>
-      <div className="list">
+    <div className='App'>
+      <div className='selected'>Selected contacts: {selected.length}</div>
+      <div className='list'>
         {data.map((personInfo) => (
           // @ts-ignore
           <PersonInfo key={personInfo.id} data={personInfo} />
