4fc03ff4a414aeebc166f7d69208572d92fc5fc4
Bartłomiej Glac <bartlomiejglac@MacBook-Pro-Bartomiej.local>
2021-07-19 21:27:33 +0200

    Adding test for load more button

--- src/ContactList.test.tsx
+++ src/ContactList.test.tsx
@@ -2,50 +2,55 @@ import {
   screen,
   render,
   waitForElementToBeRemoved,
-  wait,
   waitForElement,
+  fireEvent,
 } from "@testing-library/react";
 import React from "react";
 import App from "./App";
+import mockData from "./mockData.json";
 
-const mockPersonArray = [
-  {
-    id: "151",
-    jobTitle: "Stockbroker",
-    emailAddress: "Vera_Janes1389@corti.com",
-    firstNameLastName: "Vera Janes",
-  },
-  {
-    id: "152",
-    jobTitle: "Inspector",
-    emailAddress: "Janelle_Chappell9153@zorer.org",
-    firstNameLastName: "Janelle Chappell",
-  },
-];
-
-jest.mock("./api", () =>
-  jest.fn().mockImplementation(() => Promise.resolve(mockPersonArray))
-);
+const firstBatchOfData = mockData.slice(150, 152);
+const secondBatchOfData = mockData.slice(152, 154);
 
 afterEach(() => {
-  jest.clearAllMocks();
+  jest.resetAllMocks();
 });
 
 test("should render elements on the list when fetches successfully", async () => {
-  render(<App />);
-  await waitForElement(() => screen.getByText(mockPersonArray[0].emailAddress));
-  expect(screen.getByText(mockPersonArray[0].emailAddress)).toBeInTheDocument();
-  expect(screen.getByText(mockPersonArray[1].emailAddress)).toBeInTheDocument();
+  const mockReceiveDataFunc = jest.fn().mockResolvedValue(firstBatchOfData);
+  render(<App receiveData={mockReceiveDataFunc} />);
+  await waitForElement(() =>
+    screen.getByText(firstBatchOfData[0].emailAddress)
+  );
+  firstBatchOfData.forEach((person) =>
+    expect(screen.getByText(person.emailAddress)).toBeInTheDocument()
+  );
 });
 
 test("should render loading indicator when before fetching data", async () => {
-  render(<App />);
+  const mockReceiveDataFunc = jest.fn().mockResolvedValue(firstBatchOfData);
+  render(<App receiveData={mockReceiveDataFunc} />);
   const findLoadingElement = () => screen.getByText("Loading...");
   expect(findLoadingElement()).toBeInTheDocument();
 
   await waitForElementToBeRemoved(() => findLoadingElement());
 
-  mockPersonArray.forEach((person) =>
+  firstBatchOfData.forEach((person) =>
     expect(screen.getByText(person.emailAddress)).toBeInTheDocument()
   );
+});
+
+test("should render more elements when clicked load more button", async () => {
+  const mockReceiveDataFunc = jest
+    .fn()
+    .mockResolvedValueOnce(firstBatchOfData)
+    .mockResolvedValueOnce(secondBatchOfData);
+  render(<App receiveData={mockReceiveDataFunc} />);
+
+  await waitForElement(() =>
+    screen.getByText(firstBatchOfData[0].emailAddress)
+  );
+
+  fireEvent.click(screen.getByText("Load More"));
+  expect(screen.getAllByTestId("person-info")).toHaveLength(4);
 });
