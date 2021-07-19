9c96af517fa0aaf62eb36aa89a350561785bf2f0
Bartłomiej Glac <bartlomiejglac@MacBook-Pro-Bartomiej.local>
2021-07-19 17:46:02 +0200

    Added loading indicator before first items load

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import apiData from "./api";
 import PersonInfo from "./PersonInfo";
 
@@ -12,17 +12,21 @@ interface UserProfile {
 function App() {
   const [data, setData] = React.useState<UserProfile[]>([]);
   const [selected, setSelected] = React.useState([]);
-
+  const [isLoading, setisLoading] = useState(false);
   //  TODO fetch contacts using apiData function, handle loading and error states
   useEffect(() => {
+    setisLoading(true);
     apiData()
       .then((batch) => setData(data.concat(batch)))
-      .finally(() => {});
+      .finally(() => {
+        setisLoading(false);
+      });
   }, []);
 
   return (
     <div className='App'>
       <div className='selected'>Selected contacts: {selected.length}</div>
+      {isLoading && <div>Loading...</div>}
       <div className='list'>
         {data.map((personInfo) => (
           // @ts-ignore
